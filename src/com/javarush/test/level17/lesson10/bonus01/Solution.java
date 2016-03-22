9287dd152899a677ad95e7a6d5e6d0bec94da017
Nana <vip.excellent.2016@mail.ru>
2016-03-22 14:47:48 +0200

    Taskas about crud of persons 17 - 10 - bonus01 and bonus02

--- src/com/javarush/test/level17/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level17/lesson10/bonus01/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.test.level17.lesson10.bonus01;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /* CRUD
 CrUD - Create, Update, Delete
@@ -34,7 +37,46 @@ public class Solution {
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    public static void main(String[] args) {
-        //start here - начни тут
+    public static void main(String[] args) throws ParseException
+    {
+        SimpleDateFormat dateFormat;
+
+        if ("-c".equals(args[0])) {
+            String name = args[1];
+            Sex sex = "м".equals(args[2]) ? Sex.MALE : Sex.FEMALE;
+            dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date bd = dateFormat.parse(args[3]);
+            Person person = (sex == Sex.MALE) ? Person.createMale(name, bd) : Person.createFemale(name, bd);
+
+            allPeople.add(person);
+            System.out.println(allPeople.size()-1);
+        }
+        else if ("-u".equals(args[0])) {
+            int index = Integer.parseInt(args[1]);
+            String name = args[2];
+            Sex sex = "м".equals(args[3]) ? Sex.MALE : Sex.FEMALE;
+            dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date bd = dateFormat.parse(args[4]);
+
+            Person person = allPeople.get(index);
+            person.setName(name);
+            person.setSex(sex);
+            person.setBirthDay(bd);
+        }
+        else if ("-d".equals(args[0])) {
+            int index = Integer.parseInt(args[1]);
+            Person person = allPeople.get(index);
+            person.setName(null);
+            person.setSex(null);
+            person.setBirthDay(null);
+        }
+        else if ("-i".equals(args[0])) {
+            int id = Integer.parseInt(args[1]);
+            Person person = allPeople.get(id);
+            dateFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+            String bd = dateFormat.format(person.getBirthDay());
+
+            System.out.println(person.getName() + " " + (person.getSex() == Sex.MALE ? "м" : "ж") + " " +  bd);
+        }
     }
 }
